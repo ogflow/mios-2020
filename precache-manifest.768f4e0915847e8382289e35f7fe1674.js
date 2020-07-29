62ec0dcfa947fdacaff0e5fb060fd35a691bed19
sergey <sergey@mios.fi>
2020-07-29 13:47:58 +0300

    Updates

---
+++ precache-manifest.768f4e0915847e8382289e35f7fe1674.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68228bc8f8b65d024ab1edccafe9a745",
+    "url": "/mios-2020/index.html"
+  },
+  {
+    "revision": "8048de3fb4bca2dba067",
+    "url": "/mios-2020/static/css/2.71eaf00e.chunk.css"
+  },
+  {
+    "revision": "f5932b4e10145e458fbf",
+    "url": "/mios-2020/static/css/main.708076b4.chunk.css"
+  },
+  {
+    "revision": "8048de3fb4bca2dba067",
+    "url": "/mios-2020/static/js/2.6e6cfece.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mios-2020/static/js/2.6e6cfece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5932b4e10145e458fbf",
+    "url": "/mios-2020/static/js/main.20641263.chunk.js"
+  },
+  {
+    "revision": "e94b32b03038f34f055a",
+    "url": "/mios-2020/static/js/runtime-main.b39a4d71.js"
+  },
+  {
+    "revision": "bb761e83e24d745e618d2b697216286c",
+    "url": "/mios-2020/static/media/arrow_up_circle.bb761e83.svg"
+  },
+  {
+    "revision": "26ac17a0402b8f6598405c3c43986ed6",
+    "url": "/mios-2020/static/media/mios.26ac17a0.svg"
+  }
+]);
